e1ca6d3e84d4842fe5b40d4caba579768cdb08b6
olgatar <leyendaorama@gmail.com>
2015-12-28 01:00:31 +0100

    Final

--- js/main.js
+++ js/main.js
@@ -15,16 +15,18 @@ function storageAvailable(type) {
 
 if (storageAvailable('localStorage')) {
 
-  //Get all elements that we need
+  //Get all first elements that we need
   var todoInput = document.getElementById('todoitem');
   var addButton = document.getElementById('addButton');
   var todoList = document.getElementById('todoul');
   var finishedList = document.getElementById('finishedul');
+	var explainMessage = document.createElement("span");
+	explainMessage.innerHTML = "Click on task to change!";
 
   //Add eventlistener to addButton
   addButton.addEventListener("click", addToDoTask);
 
-  //Function to add a task to ToDo list
+  //Function to add tasks to ToDo list
   function addToDoTask(){
     if (todoInput.value !== "") {
       var todoli = document.createElement("li");
@@ -32,57 +34,50 @@ if (storageAvailable('localStorage')) {
 
       var buttonDiv = document.createElement("div");
       todoli.className = "list-group-item";
-
       todoliText.innerHTML = todoInput.value;
 
       var deleteButton = document.createElement("button");
           deleteButton.innerHTML = "Delete";
           deleteButton.style.color = "white";
-          deleteButton.setAttribute("onclick",deleteToDoTask);
+          deleteButton.setAttribute("onclick","deleteToDoTask.call(this)");
 
       var changeButton = document.createElement("button");
           changeButton.innerHTML = "Change";
           changeButton.style.color = "white";
-          changeButton.setAttribute("onclick",changeToDoTask);
+          changeButton.setAttribute("onclick","changeToDoTask.call(this)");
 
       var saveChangeButton = document.createElement("button");
           saveChangeButton.innerHTML = "Save changes";
           saveChangeButton.style.color = "white";
-          saveChangeButton.setAttribute("onclick",saveChangeToDoTask);
+          saveChangeButton.setAttribute("onclick","saveChangeToDoTask.call(this)");
 
       var finishedButton = document.createElement("button");
           finishedButton.innerHTML = "Finished";
           finishedButton.style.color = "white";
-          finishedButton.setAttribute("onclick",finishedToDoTask);
-
+          finishedButton.setAttribute("onclick","finishedToDoTask.call(this)");
 
       //Add eventlistener to new buttons
       deleteButton.addEventListener("click", deleteToDoTask);
       changeButton.addEventListener("click", changeToDoTask);
       saveChangeButton.addEventListener("click", saveChangeToDoTask);
-      finishedButton.addEventListener("click", finishedToDoTask);
-
+    	finishedButton.addEventListener("click", finishedToDoTask);
 
       //Append all elements to ToDoList
       buttonDiv.appendChild(changeButton);
       buttonDiv.appendChild(saveChangeButton);
       buttonDiv.appendChild(finishedButton);
       buttonDiv.appendChild(deleteButton);
+			buttonDiv.appendChild(explainMessage);
+			buttonDiv.lastChild.style.visibility = "hidden";
       todoli.appendChild(todoliText);
       todoli.appendChild(buttonDiv);
       todoList.appendChild(todoli);
       todoInput.value ="";
 
-      return deleteButton;
-      return changeButton;
-      return saveChangeButton;
-      return finishedButton;
-
-      saveToLocalStorage();
+			//Call Function to save data to local storage
+			saveToLocalStorage();
     }
-    saveToLocalStorage();
   }
-
 
   //Function to delete the task that has been added to ToDo list
   function deleteToDoTask(){
@@ -90,29 +85,40 @@ if (storageAvailable('localStorage')) {
     var deleteFirstDiv = this.parentNode.previousSibling;
     deleteLastDiv.remove();
     deleteFirstDiv.parentNode.remove();
+
+		//Call Function to save data to local storage
     saveToLocalStorage();
   }
 
   //Function to change the task that has been added to ToDo list
   function changeToDoTask(){
+		var changeToDoTaskFirstDiv = this.parentNode;
     var changeToDoTask = this.parentNode.previousSibling;
     changeToDoTask.contentEditable="true";
+		changeToDoTaskFirstDiv.lastChild.style.visibility = "visible";
     changeToDoTask.style.color = "red";
+
+		//Call Function to save data to local storage
     saveToLocalStorage();
   }
 
   //Function to save changes to the task that has been added to ToDo list
   function saveChangeToDoTask(){
+		var changeToDoTaskFirstDiv = this.parentNode;
     var changeToDoTask = this.parentNode.previousSibling;
     changeToDoTask.contentEditable="false";
     changeToDoTask.style.color = "black";
+		changeToDoTaskFirstDiv.lastChild.style.visibility = "hidden";
+		
+		//Call Function to save data to local storage
     saveToLocalStorage();
   }
 
-  //Function to delete the task that has been added to ToDo list
+  //Function to move the task to Finished ToDo list
   function finishedToDoTask(){
     var finishedLastDiv = this.parentNode;
     var finishedFirstDiv = this.parentNode.previousSibling;
+		finishedFirstDiv.style.color = "black";
     for (var i=0; i<finishedLastDiv.childNodes.length; i++) {
       finishedLastDiv.removeChild(finishedLastDiv.childNodes[i]);
      }
@@ -120,27 +126,36 @@ if (storageAvailable('localStorage')) {
        finishedLastDiv.removeChild(finishedLastDiv.childNodes[i]);
      }
     finishedList.appendChild(finishedFirstDiv.parentNode);
+
+		//Call Function to save data to local storage
     saveToLocalStorage();
   }
 
+	//Function to save data to local storage
   function saveToLocalStorage() {
-    var test1 = JSON.stringify(todoList.innerHTML);
-    localStorage.todoListStorage = test1;
-    //localStorage.setItem("finishedListStorage", JSON.stringify(finishedList.innerHTML));
+		if(todoList.innerHTML != "undefined" && todoList.innerHTML != null){
+			localStorage.todo = JSON.stringify(todoList.innerHTML);
+		}
+		if(finishedList.innerHTML != "undefined" && finishedList.innerHTML != null){
+		localStorage.finished = JSON.stringify(finishedList.innerHTML);
+		}
   }
 
+	//Function to get data from local storage
   function getFromLocalStorage() {
-    //var todoListGetStorage = localStorage.getItem("todoListStorage");
-    todoList.innerHTML = localStorage.todoListStorage;
-    //var finishedListGetStorage = localStorage.getItem("finishedListStorage");
-    //finishedList.innerHTML = JSON.parse(finishedListGetStorage);
+		if(todoList.innerHTML != "undefined" && todoList.innerHTML != null){
+		todoList.innerHTML = JSON.parse(localStorage.todo);
+
+		}
+		if(finishedList.innerHTML != "undefined" && finishedList.innerHTML != null){
+			finishedList.innerHTML = JSON.parse(localStorage.finished);
+		}
   }
 
-window.onload = function() {
-  getFromLocalStorage();
+	//Function to get data from local storage
+	getFromLocalStorage();
 
-}
 }
 else {
-alert("No Local Storage!");
+	alert("No Local Storage!");
 }
